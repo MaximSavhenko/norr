b21e6186829d9578dc353f8f70f501e5c0b90aa2
Maxim <uspmsm@gmail.com>
2025-08-16 17:53:28 +0300

    fix: - error versal / [id]/page (params)

--- norr-frontend/src/app/(app)/topics/[id]/page.tsx
+++ norr-frontend/src/app/(app)/topics/[id]/page.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from 'next'
 
+import { CheckListCard } from '@/components/check-list/CheckListCard'
 import { NoteCard } from '@/components/notes/NoteCard'
 import { NoteCreate } from '@/components/notes/NoteCreate'
-import { CheckListCard } from '@/components/check-list/CheckListCard'
 
 export const metadata: Metadata = {
 	title: 'Topics'
 }
 
 interface ITopicPage {
-	params: {
+	params: Promise<{
 		id: string
-	}
+	}>
 }
 
 export default async function TopicPage({ params }: ITopicPage) {
@@ -19,8 +19,11 @@ export default async function TopicPage({ params }: ITopicPage) {
 
 	return (
 		<div>
-			<NoteCreate topicId={id}/>
-			<CheckListCard className='mb-4' topicId={id}/>
+			<NoteCreate topicId={id} />
+			<CheckListCard
+				className='mb-4'
+				topicId={id}
+			/>
 			<NoteCard id={id} />
 		</div>
 	)
